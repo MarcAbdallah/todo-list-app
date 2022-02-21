980b10cfc098fb2e220d5f8abb5369d55f97cb48
logajame13 <logan.v.james13@gmail.com>
2022-02-21 10:10:10 -0600

    RecyclerView

---
+++ todolistapp/app/src/main/java/com/example/todo_list_app/Data/DataSource.kt
@@ -0,0 +1,20 @@
+package com.example.todo_list_app.Data
+
+import com.example.todo_list_app.R
+import com.example.todo_list_app.model.Weekdays
+
+class DataSource {
+    fun loadWeekdays(): List<Weekdays>{
+        return listOf<Weekdays>(
+            Weekdays(R.string.Monday),
+            Weekdays(R.string.Tuesday),
+            Weekdays(R.string.Wednesday),
+            Weekdays(R.string.Thursday),
+            Weekdays(R.string.Friday),
+            Weekdays(R.string.Saturday),
+            Weekdays(R.string.Sunday)
+
+        )
+
+    }
+}
---
+++ todolistapp/app/src/main/java/com/example/todo_list_app/model/Weekdays.kt
@@ -0,0 +1,4 @@
+package com.example.todo_list_app.model
+
+data class Weekdays (val stringResourceId: Int) {
+}
